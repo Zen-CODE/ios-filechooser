599ccfd2ba2915ddfac2a0bc77aff92d7f8830b9
richard <richard@dotmodus>
2020-02-08 12:06:27 +0200

    Start with move to python delegate

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ sys.path.append("/".join(x for x in __file__.split("/")[:-1]))
 from kivy.app import App
 from kivy.utils import platform
 if platform == 'ios':
-    from pyobjus import autoclass
+    from pyobjus import autoclass, protocol
 from kivy.uix.image import Image
 from kivy.properties import ObjectProperty, StringProperty
+from pyobjus.dylib_manager import load_framework
 
 
 
@@ -27,7 +28,28 @@ class MainApp(App):
 
 
     def pick_image(self):
-        if platform == 'ios':
-            self.native_image_picker.displayImagePicker()
+        # if platform == 'ios':
+        #     self.native_image_picker.displayImagePicker()
+        picker = autoclass("UIImagePickerController")
+        print("Created UIImagePickerController..")
+
+        picker_object = picker.alloc().init()
+        print(f"picker_object = {picker_object}")
+
+        # picker_object.sourceType = picker_object.SourceType.photoLibrary
+        picker_object.delegate = self
+
+        load_framework('/System/Library/Frameworks/Photos.framework')
+        print("loaded Photolib framework..")
+
+    @protocol('UIImagePickerControllerDelegate')
+    def imagePickerController_didFinishPickingMediaWithInfo_(self, *args):
+        print("UIImagePickerControllerDelegate called")
+
+    # @protocol("didFinishPickingMediaWithInfo")
+    # def arb_name(self, *args):
+    #     pass
+
+
 
 MainApp().run()
