07cdf51a325147ecaaf1485cab2914a81319c4b4
richard <richard@dotmodus>
2020-02-15 15:04:56 +0200

    :zap: Showing file picker

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from kivy.uix.image import Image
 from kivy.properties import ObjectProperty, StringProperty
 from pyobjus.dylib_manager import load_framework
 
-
+load_framework('/System/Library/Frameworks/Photos.framework')
 
 class MainApp(App):
     native_image_picker = ObjectProperty(None)
@@ -17,6 +17,7 @@ class MainApp(App):
     def on_start(self):
         if platform == 'ios':
             self.native_image_picker = autoclass("NativeImagePicker").alloc().init()
+        pass
 
     def update(self):
         print("Updating image...")
@@ -26,33 +27,26 @@ class MainApp(App):
         image = Image(source=image_path, nocache=True)
         self.root.add_widget(image)
 
+    def callback(self):
+        print("Callback fired!!!! ")
 
     def pick_image(self):
         # if platform == 'ios':
+        #     print("Callign objective C picker")
+        #     self.native_image_picker.callback = lambda: self.callback()
         #     self.native_image_picker.displayImagePicker()
-        picker = autoclass(
-            "UIImagePickerController",
-            load_class_methods=[b'alloc'],
-            load_instance_methods=[b'init', b'setSourceType_'])
+        #     return
 
+        picker = autoclass("UIImagePickerController",
+                           copy_properties=False)
         picker_object = picker.alloc().init()
-        print(f"picker_object = {picker_object}")
 
-        # picker_object.sourceType = objc_i(0) - give  TypeError: an integer
-        # is required (got type __NSCFNumber)
         picker_object.sourceType = 0 
-        print("picker_object.sourceType = objc_i(0)")
-
-        # picker_object.sourceType = picker_object.SourceType.photoLibrary
         picker_object.delegate = self
-        print("Picker delegate set")
 
         UIApplication = autoclass('UIApplication')
         vc = UIApplication.sharedApplication().keyWindow.rootViewController()
-        print(f"vc = {vc}")
 
-        # vc.presentViewController_animated_completion_(picker, objc_b(True), objc_b(True))
-        # vc.presentViewController_animated_completion_(picker, True, ObjcVoid(None))
         vc.presentViewController_animated_completion_(picker_object, True, None)
         print("Called vc.presentViewController_animated_completion_")
 
