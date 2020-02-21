14f4fbc1cb433e61dc390b05822913b6c145445a
richard <richard@dotmodus>
2020-02-21 22:20:25 +0200

    :construction: Cleaned up NativeImagePicker implementation

--- main.m
+++ main.m
@@ -1,7 +1,4 @@
-#import <Photos/Photos.h>
-
-@interface NativeImagePicker : UIViewController
-@property (strong, nonatomic) UIImagePickerController *imagePickerController;
+@interface NativeImagePicker : NSObject
 @end
 @implementation NativeImagePicker
 
@@ -10,44 +7,12 @@
     return self;
 }
 
-- (int)displayImagePicker {
-    PHAuthorizationStatus status = [PHPhotoLibrary authorizationStatus];
-    if(status == PHAuthorizationStatusNotDetermined) {
-        // Request photo authorization
-        [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
-            // User code (show imagepicker)
-        }];
-    } else if (status == PHAuthorizationStatusAuthorized) {
-        // User code
-    } else if (status == PHAuthorizationStatusRestricted) {
-        // User code
-    } else if (status == PHAuthorizationStatusDenied) {
-        // User code
-    }
-    UIImagePickerController* imagePicker = [[UIImagePickerController alloc]init];
-    // Check if image access is authorized
-    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
-        imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-        // Use delegate methods to get result of photo library -- Look up UIImagePicker delegate methods
-        imagePicker.delegate = self;
-        // Must show our imagepicker above the root view
-        UIViewController *top = [UIApplication sharedApplication].keyWindow.rootViewController;
-        [top presentViewController:imagePicker animated:YES completion: nil];
-    }
-    return 0;
-}
-
 - (NSString*) getFileName:(NSURL *) ns_url {
     // Return the file name without the path or file extention
     NSString *image_name = ns_url.pathComponents.lastObject;
     NSArray *listItems = [image_name componentsSeparatedByString:@"."];
     NSString *ret = listItems[0];
     return ret;
-}
-
-- (NSString*) getPNGFile {
-    NSString* test = @"My NSString";
-    return test;
 }
 
 - (NSString*) writeToPNG: (NSDictionary *) info {
@@ -77,21 +42,5 @@
         NSLog(@"the cachedImagedPath is %@",imagePath);
         return imagePath;
     }
-
 }
-
-- (void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
-    // Save the image as "cached.png" in a folder accessible by the app (user_data_dir in python/kivy)
-    // NSURL *ns_url = info[UIImagePickerControllerImageURL];
-    // NSString *image_name = ns_url.pathComponents.lastObject;
-    // NSArray *listItems = [image_name componentsSeparatedByString:@"."];
-    // image_name = listItems[0];
-    NSString *png_path = [self writeToPNG:info fileName: @"cached"];
-    NSLog(@"Final image is %@", png_path);
-
-    [picker dismissViewControllerAnimated:YES completion:nil];//{
-    // <#code#>
-    //}];
-}
-
 @end
